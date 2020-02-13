80173c0f2b5ed53e0d009260605ee10e6e6ee503
wesley.moody <wgm3190@gmail.com>
2020-02-13 12:48:19 -0600

    save

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
 server.use(express.json());
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   const songs = [{ id: 1, name: "Final Countdown" }];
 
   res.status(200).json(songs);
 });
 
 // make the port dynamic
-const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`\n** Running on port${port} **\n`))
-
-
-
+const port = process.env.PORT || 5005;
+server.listen(port, () => console.log(`\n** Running on port${port} **\n`));
